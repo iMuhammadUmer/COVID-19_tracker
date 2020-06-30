d91a552a62b0595f16997f9eb52f7714e773b20f
Muhammad Umer <mumer1297@gmail.com>
2020-06-30 15:23:18 +0500

    added Information.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import "./Header";
 import { Header } from "./Header";
+import { Information } from "./Information";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Information />
       </header>
     </div>
   );
