3b8ce2087f6a4a61d30161a6a20128c2569eb71f
Muhammad Umer <mumer1297@gmail.com>
2020-06-30 23:24:31 +0500

    [update] importing components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import "./Header";
 import { Header } from "./Header";
-import { Information } from "./Information";
+import MainGrid from "./components/MainGrid";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <Information />
+        <MainGrid />
       </header>
     </div>
   );
