7475de10118d19e75863a7b3c15804d23fffb589
Muhammad Umer <mumer1297@gmail.com>
2020-07-02 20:35:42 +0500

    update grid columns

--- src/components/CountryStats.js
+++ src/components/CountryStats.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import { CountryList } from "./CountryList";
+import CountryList from "./CountryList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,12 +21,12 @@ export const CountryStats = () => {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={5}>
           <Paper className={classes.paper}>
             <CountryList />
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={8}>
+        <Grid item xs={12} sm={7}>
           <Paper className={classes.paper}>xs=12 sm=6</Paper>
         </Grid>
       </Grid>
