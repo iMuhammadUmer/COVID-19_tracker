d279cb539901204b213ec46a244e91c85b099fa7
Muhammad Umer <mumer1297@gmail.com>
2020-07-03 06:23:39 +0500

    [WIP] line graph

--- src/components/WorldWideGraphOverTime.js
+++ src/components/WorldWideGraphOverTime.js
@@ -1,42 +1,86 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
+import {
+  makeStyles,
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+} from "@material-ui/core";
 
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "My First dataset",
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40],
-    },
-  ],
-};
-
-export default React.createClass({
-  displayName: "LineExample",
-
-  render() {
-    return (
-      <div>
-        <h2>Line Example</h2>
-        <Line data={data} />
-      </div>
-    );
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    backgroundColor: "blue",
+    border: 1,
+    m: 1,
+    borderColor: "white",
+    color: "white",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  pos: {
+    color: "black",
+    letterSpacing: 3,
+  },
+  grid: {
+    width: "95%",
+    marginTop: "1em",
+  },
+  leftCard: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+    textAlign: "left",
+    marginTop: "1em",
+  },
+  totalCases: {
+    marginTop: "0.5em",
+  },
+  worldGraph: {
+    width: 50,
+    height: 50,
+  },
+  countryStats: {
+    marginTop: "1em",
+  },
+  detailedStats: {
+    width: 200,
+    height: 100,
+    borderRadius: 10,
+    padding: 2,
+    border: 1,
+    m: 1,
+    borderColor: "black",
   },
 });
+
+export const WorldWideGraphOverTime = () => {
+  const classes = useStyles();
+  return (
+    <Card
+      className={classes.leftCard}
+      variant="outlined"
+      style={{ height: 300 }}
+    >
+      <CardContent>
+        <div className={classes.totalCases}>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            World wide graph over time
+          </Typography>
+          <h3>Line graph</h3>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
