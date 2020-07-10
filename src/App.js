c9b5fd10f28450d8cc913e3d8dd7491e9ca366ee
Muhammad Umer <mumer1297@gmail.com>
2020-07-10 21:08:22 +0500

    import components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from "react";
-import "./App.css";
-import { Header } from "./components/Header";
-import MainGrid from "./components/MainGrid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <MainGrid />
-      </header>
-    </div>
-  );
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
+import styles from "./App.module.css";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.h1}>COVID - 19</h1>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
